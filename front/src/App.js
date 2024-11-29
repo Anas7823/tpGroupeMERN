5cb5e54629359106322eb924e57d6e38e7ddd4de
Anas7823 <anaselkhiat78@gmail.com>
2024-11-29 10:14:40 +0100

    debug

--- front/src/App.js
+++ front/src/App.js
@@ -2,10 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
-import Home from './Components/Home/Home'
-import FeaturedProducts from "./Components/FeaturedProducts/FeaturedProducts";
+// import Home from './Components/Home/Home'
+// import FeaturedProducts from "./Components/FeaturedProducts/FeaturedProducts";
 import Login from './Components/Login/Login';
 import Register from './Components/Register/Register';
+import Produits from './Components/Produits/Produits';
 
 
 function App() {
@@ -13,7 +14,8 @@ function App() {
     <BrowserRouter>
     <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        {/* <Route path="/" element={<Home />} /> */}
+        <Route path="/produits" element={<Produits />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ front/src/Components/Produits/Produits.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Produits.css'
+
+function Produits() {
+    return (
+        <div className='Produits'>
+
+        </div>
+    )
+}
+
+export default Produits
