50c486d8b2c47854d8a871aa2cb7d1ebf476c7ee
Anas7823 <anaselkhiat78@gmail.com>
2024-11-29 09:41:00 +0100

    changement port

--- back/app.js
+++ back/app.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 app.use(express.json());
 app.use(cors({ origin: "*" }));
 
-const PORT = 8080;
+const PORT = 3002;
 
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/tpgroupe", {});
